0246019179fe6ee809b8d608cda7a2b831327d08
Chitandaaaaa <707320242@qq.com>
2019-11-23 02:54:45 +0800

    账号关联成功了‘ ’

--- src/main/java/utils/test.java
+++ src/main/java/utils/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("push测试？");
+		System.out.println("key账号关联成功了！");
     }
 }
